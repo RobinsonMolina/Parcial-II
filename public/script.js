ab915f29bfa71d0dafc78aa2ed30f3ed2116b27f
ROBINSON MOLINA <robinson.granados01@gmail.com>
2024-10-08 19:49:02 -0500

    Finished Parcial-II

--- public/script.js
+++ public/script.js
@@ -1,7 +1,5 @@
 document.addEventListener('DOMContentLoaded', async () => {
-
     await loadCharacters();
-
 
     document.getElementById('searchId').addEventListener('click', async () => {
         const id = document.getElementById('idSearch').value.trim();
@@ -10,12 +8,15 @@ document.addEventListener('DOMContentLoaded', async () => {
         }
     });
 
+    document.getElementById('viewAll').addEventListener('click', async () => {
+        await loadCharacters();
+    });
 
     document.getElementById('nameSearch').addEventListener('input', filterCharacters);
 });
 
 async function loadCharacters() {
-    const response = await fetch('/api/characters');
+    const response = await fetch('/api/characters'); 
     const characters = await response.json();
     displayCharacters(characters);
 }
@@ -23,7 +24,6 @@ async function loadCharacters() {
 function displayCharacters(characters) {
     const characterList = document.getElementById('characterList');
     characterList.innerHTML = '';
-
     characters.forEach(character => {
         const row = document.createElement('tr');
         row.innerHTML = `
@@ -46,7 +46,6 @@ function displayCharacters(characters) {
 async function searchCharacterById(id) {
     const response = await fetch(`https://starwars-n5ec-developuptcs-projects.vercel.app/${id}`);
     const data = await response.json();
-    
     if (data.result) {
         displayCharacters([data.data]);
     } else {
@@ -57,7 +56,6 @@ async function searchCharacterById(id) {
 function filterCharacters() {
     const query = document.getElementById('nameSearch').value.toLowerCase();
     const characterRows = document.querySelectorAll('#characterList tr');
-
     characterRows.forEach(row => {
         const nameCell = row.cells[1].textContent.toLowerCase();
         row.style.display = nameCell.includes(query) ? '' : 'none';
